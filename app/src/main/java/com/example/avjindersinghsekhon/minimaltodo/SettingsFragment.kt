507cfcd34ab3b224656a060cd602b8b06c278e19
kgj1132 <kgj1132@gmail.com>
2017-08-06 13:44:18 +0900

    SettingsFragment 변경

---
+++ app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/SettingsFragment.kt
@@ -0,0 +1,44 @@
+package com.example.avjindersinghsekhon.minimaltodo
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.preference.CheckBoxPreference
+import android.preference.PreferenceFragment
+
+class SettingsFragment : PreferenceFragment(), SharedPreferences.OnSharedPreferenceChangeListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addPreferencesFromResource(R.xml.preferences_layout)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+        val preferenceKeys = PreferenceKeys(resources)
+        if (key == preferenceKeys.night_mode_pref_key) {
+            val themePreferences = activity.getSharedPreferences(MainActivity.THEME_PREFERENCES, Context.MODE_PRIVATE)
+            val themeEditor = themePreferences.edit()
+            themeEditor.putBoolean(MainActivity.RECREATE_ACTIVITY, true)
+
+            val checkBoxPreference = findPreference(preferenceKeys.night_mode_pref_key) as CheckBoxPreference
+            if (checkBoxPreference.isChecked) {
+                themeEditor.putString(MainActivity.THEME_SAVED, MainActivity.DARKTHEME)
+            } else {
+                themeEditor.putString(MainActivity.THEME_SAVED, MainActivity.LIGHTTHEME)
+            }
+            themeEditor.apply()
+
+            activity.recreate()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+    }
+}
