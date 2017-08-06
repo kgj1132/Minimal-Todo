ed689eb4b041c613df875cfe7f3593f52c96dd97
kgj1132 <kgj1132@gmail.com>
2017-08-06 18:24:32 +0900

    PreferenceKey 변경

---
+++ app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/PreferenceKeys.kt
@@ -0,0 +1,8 @@
+package com.example.avjindersinghsekhon.minimaltodo
+
+import android.content.res.Resources
+
+class PreferenceKeys(resources: Resources) {
+    @JvmField
+    var night_mode_pref_key : String = resources.getString(R.string.night_mode_pref_key)
+}
