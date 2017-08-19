09532cb72e8004ef54fd01c92c3500e49ad2d11b
kgj1132 <kgj1132@gmail.com>
2017-08-19 22:38:10 +0900

    DeleteNotificationService 변경

---
+++ app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/DeleteNotificationService.kt
@@ -0,0 +1,71 @@
+package com.example.avjindersinghsekhon.minimaltodo
+
+import android.app.IntentService
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import org.json.JSONException
+import java.io.IOException
+
+import java.util.ArrayList
+import java.util.UUID
+
+class DeleteNotificationService : IntentService("DeleteNotificationService") {
+
+    private var storeRetrieveData: StoreRetrieveData? = null
+    private var mToDoItems: ArrayList<ToDoItem?>? = null
+    private var mItem: ToDoItem? = null
+
+    override fun onHandleIntent(intent: Intent?) {
+        storeRetrieveData = StoreRetrieveData(this, MainActivity.FILENAME)
+        val todoID = intent!!.getSerializableExtra(TodoNotificationService.TODOUUID) as UUID
+
+        mToDoItems = loadData()
+        mItem = mToDoItems!!.filter { item -> item?.identifier == todoID }.firstOrNull()
+        mItem.let {
+            mToDoItems!!.remove(mItem!!)
+            dataChanged()
+            saveData()
+        }
+    }
+
+    private fun dataChanged() {
+        baseContext.getSharedPreferences(MainActivity.SHARED_PREF_DATA_SET_CHANGED, Context.MODE_PRIVATE)
+                .edit().run { putBoolean(MainActivity.CHANGE_OCCURED, true) }.apply()
+    }
+
+    private fun saveData() {
+        try {
+            storeRetrieveData!!.saveToFile(mToDoItems)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        saveData()
+    }
+
+    private fun loadData(): ArrayList<ToDoItem?> {
+        var loadData: ArrayList<ToDoItem?> = ArrayList<ToDoItem?>(0)
+        try {
+            loadData = storeRetrieveData!!.loadFromFile()
+
+            if (loadData == null)
+                throw KotlinNullPointerException()
+        } catch(e: Exception) {
+
+            when (e) {
+                is KotlinNullPointerException,
+                is JSONException,
+                is IOException -> {
+                    e.printStackTrace()
+                }
+                else -> throw e
+            }
+        }
+        return loadData
+    }
+}
